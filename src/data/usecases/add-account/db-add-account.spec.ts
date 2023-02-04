6f989171103bdc0f6a815970cae3e4e8b7feedb8
Pvstelles <paulo.telles@rockapps.com.br>
2023-02-04 09:29:42 -0300

    test: ensure DbAddAccount return an account on success

--- src/data/usecases/add-account/db-add-account.spec.ts
+++ src/data/usecases/add-account/db-add-account.spec.ts
@@ -93,4 +93,19 @@ describe('DbAddAccount Use case', () => {
     const promisse = sut.add(account)
     await expect(promisse).rejects.toThrow()
   })
+  test('Should DbAddAccount return an account on success', async () => {
+    const { sut } = makeSut()
+    const accountData = {
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'valid_password'
+    }
+    const account = await sut.add(accountData)
+    expect(account).toEqual({
+      id: 'valid_id',
+      name: 'valid_name',
+      email: 'valid_email',
+      password: 'hashed_password'
+    })
+  })
 })
