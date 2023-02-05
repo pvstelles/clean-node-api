511f37141b914fe5305464a6461a3e35e2de9799
Pvstelles <pvstelles@gmail.com>
2023-02-05 17:37:07 -0300

    feat: add login route

--- src/main/routes/login-routes.ts
+++ src/main/routes/login-routes.ts
@@ -1,8 +1,12 @@
 import { type Router } from 'express'
 import { makeSignUpController } from '../factories/signup/signup-factory'
+import { makeLoginController } from '../factories/login/login-factory'
 import { adaptRoute } from '../adapters/express/express-routes-adapter'
 
 export default (router: Router): void => {
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   router.post('/signup', adaptRoute(makeSignUpController()))
+
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  router.post('/login', adaptRoute(makeLoginController()))
 }
