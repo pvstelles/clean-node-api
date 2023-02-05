d05343136a00c4bd08c9a63a6ec154f8750c8383
Pvstelles <pvstelles@gmail.com>
2023-02-05 17:37:19 -0300

    test: add login route

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -2,6 +2,10 @@ import request from 'supertest'
 
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
+import { type Collection } from 'mongodb'
+import { hash } from 'bcrypt'
+
+let accountCollection: Collection
 describe('Login Routes', () => {
   beforeAll(async () => {
     // @ts-expect-error error no typescript
@@ -11,7 +15,7 @@ describe('Login Routes', () => {
     await MongoHelper.disconnect()
   })
   beforeEach(async () => {
-    const accountCollection = await MongoHelper.getCollection('accounts')
+    accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
 
@@ -24,6 +28,24 @@ describe('Login Routes', () => {
           email: 'paulo.telles@rockapps.com.br',
           password: 'paulo',
           passwordConfirmation: 'paulo'
+        })
+        .expect(200)
+    })
+  })
+  describe('POST /login', () => {
+    test('Should return status code 200 on signup', async () => {
+      const passwordHashed = await hash('paulo', 12)
+      await accountCollection.insertOne({
+        name: 'Paulo Victor',
+        email: 'paulo.telles@rockapps.com.br',
+        password: passwordHashed,
+        passwordConfirmation: passwordHashed
+      })
+      await request(app)
+        .post('/api/login')
+        .send({
+          email: 'paulo.telles@rockapps.com.br',
+          password: 'paulo'
         })
         .expect(200)
     })
