ca02d668ed7c35d0c481801ba731c03141e6bf88
Pvstelles <pvstelles@gmail.com>
2023-02-05 15:24:46 -0300

    test: ensure BcryptAdapter return false when Bcrypt.compare is fail

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -48,4 +48,13 @@ describe('Bcrypt Adapter', () => {
     const isValid = await sut.compare('any_value', 'any_hash')
     expect(isValid).toBeTruthy()
   })
+
+  test('Should return false if bcrypt.compare fail', async () => {
+    const sut = makeSut()
+    jest.spyOn(bcrypt, 'compare').mockImplementationOnce(() => {
+      return false
+    })
+    const isValid = await sut.compare('any_value', 'any_hash')
+    expect(isValid).toBe(false)
+  })
 })
