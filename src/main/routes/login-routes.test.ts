24283f6b00197b99a5c2642a2aff9977b54f1078
Pvstelles <pvstelles@gmail.com>
2023-02-05 17:14:34 -0300

    refactor: rename signup routes to login routes

--- src/main/routes/login-routes.test.ts
+++ src/main/routes/login-routes.test.ts
@@ -2,7 +2,7 @@ import request from 'supertest'
 
 import app from '../config/app'
 import { MongoHelper } from '../../infra/db/mongodb/helpers/mongo-helper'
-describe('SignUp Routes', () => {
+describe('Login Routes', () => {
   beforeAll(async () => {
     // @ts-expect-error error no typescript
     await MongoHelper.connect(process.env.MONGO_URL)
@@ -14,15 +14,18 @@ describe('SignUp Routes', () => {
     const accountCollection = await MongoHelper.getCollection('accounts')
     await accountCollection.deleteMany({})
   })
-  test('Should return an account on success', async () => {
-    await request(app)
-      .post('/api/signup')
-      .send({
-        name: 'Paulo Victor',
-        email: 'paulo.telles@rockapps.com.br',
-        password: 'paulo',
-        passwordConfirmation: 'paulo'
-      })
-      .expect(200)
+
+  describe('POST /signup', () => {
+    test('Should return status code 200 on signup', async () => {
+      await request(app)
+        .post('/api/signup')
+        .send({
+          name: 'Paulo Victor',
+          email: 'paulo.telles@rockapps.com.br',
+          password: 'paulo',
+          passwordConfirmation: 'paulo'
+        })
+        .expect(200)
+    })
   })
 })
