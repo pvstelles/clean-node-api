5a9bb870f1e7c468c89fbea9ee5ee9bc63c057aa
Pvstelles <pvstelles@gmail.com>
2023-02-05 17:10:24 -0300

    refactor: move email validator adapter to the main layer

--- src/main/routes/signup-routes.ts
+++ src/main/routes/signup-routes.ts
@@ -1,6 +1,6 @@
 import { type Router } from 'express'
 import { makeSignUpController } from '../factories/signup/signup-factory'
-import { adaptRoute } from '../adapters/express-routes-adapter'
+import { adaptRoute } from '../adapters/express/express-routes-adapter'
 
 export default (router: Router): void => {
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
