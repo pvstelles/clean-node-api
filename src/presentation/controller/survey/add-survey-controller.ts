b0905b368974d95fdbb0d27b7b6cad9598ceb46c
Pvstelles <pvstelles@gmail.com>
2023-02-07 12:28:08 -0300

    feat: ensure AddSurveyController return serverError if AddSurvey.add return throws

--- src/presentation/controller/survey/add-survey-controller.ts
+++ src/presentation/controller/survey/add-survey-controller.ts
@@ -1,6 +1,6 @@
 import { type Controller, type HttpRequest, type HttpResponse } from '../../protocols'
 import { type Validation } from '../../../validation/protocols'
-import { badRequest } from '../../helpers/http/http-helper'
+import { badRequest, serverError } from '../../helpers/http/http-helper'
 import { type AddSurvey } from '../../../domain/usecases/add-survey'
 
 export class AddSurveyController implements Controller {
@@ -11,12 +11,16 @@ export class AddSurveyController implements Controller {
   }
 
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
-    const error = this.validation.validate(httpRequest.body)
-    if (error) {
-      return badRequest(error)
+    try {
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
+      }
+      const { question, answers } = httpRequest.body
+      await this.addSurvey.add({ question, answers })
+      return { body: {}, statusCode: 200 }
+    } catch (e) {
+      return serverError(e)
     }
-    const { question, answers } = httpRequest.body
-    await this.addSurvey.add({ question, answers })
-    return { body: {}, statusCode: 200 }
   }
 }
