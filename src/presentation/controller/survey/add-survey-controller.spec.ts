266e3f9dd2d3b8b62ee9df7eb7b9badae426cc7e
Pvstelles <pvstelles@gmail.com>
2023-02-07 12:10:24 -0300

    test: ensure AddSurveyController return a badRequest error if Validation fails

--- src/presentation/controller/survey/add-survey-controller.spec.ts
+++ src/presentation/controller/survey/add-survey-controller.spec.ts
@@ -1,5 +1,6 @@
 import { type HttpRequest, type Validation } from './add-survey-controller-protocols'
 import { AddSurveyController } from './add-survey-controller'
+import { badRequest } from '../../helpers/http/http-helper'
 
 const makeFakeRequest = (): HttpRequest => {
   return {
@@ -40,11 +41,11 @@ describe('Add Survey Controller', () => {
     await sut.handle(httpRequest)
     expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
   })
-  test('Should call Validation with correct values', async () => {
+  test('Should return 500 if Validation Fails', async () => {
     const { sut, validationStub } = makeSut()
-    const validateSpy = jest.spyOn(validationStub, 'validate')
+    jest.spyOn(validationStub, 'validate').mockReturnValueOnce(new Error())
     const httpRequest = makeFakeRequest()
-    await sut.handle(httpRequest)
-    expect(validateSpy).toHaveBeenCalledWith(httpRequest.body)
+    const httpResponse = await sut.handle(httpRequest)
+    expect(httpResponse).toEqual(badRequest(new Error()))
   })
 })
