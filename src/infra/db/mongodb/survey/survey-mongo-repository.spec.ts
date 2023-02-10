a2b638341cd151bdace79b475a82fa2758b2a826
Pvstelles <pvstelles@gmail.com>
2023-02-10 11:45:12 -0300

    test: ensure SurveyMongoRepository add a new Survey on add success

---
+++ src/infra/db/mongodb/survey/survey-mongo-repository.spec.ts
@@ -0,0 +1,35 @@
+import { MongoHelper } from '../helpers/mongo-helper'
+import { type Collection } from 'mongodb'
+import { SurveyMongoRepository } from './survey-mongo-repository'
+
+describe('Survey Mongo Repository', () => {
+  let surveyCollection: Collection
+  beforeAll(async () => {
+    // @ts-expect-error error no typescript
+    await MongoHelper.connect(process.env.MONGO_URL)
+  })
+  afterAll(async () => {
+    await MongoHelper.disconnect()
+  })
+  beforeEach(async () => {
+    surveyCollection = await MongoHelper.getCollection('surveys')
+    await surveyCollection.deleteMany({})
+  })
+
+  const makeSut = (): SurveyMongoRepository => {
+    return new SurveyMongoRepository()
+  }
+
+  test('Should return an survey on success', async () => {
+    const sut = makeSut()
+    await sut.add({
+      question: 'any_question',
+      answers: [
+        { image: 'any_image', answer: 'any_answer' },
+        { answer: 'any_answer' }
+      ]
+    })
+    const survey = await surveyCollection.findOne({ question: 'any_question' })
+    expect(survey).toBeTruthy()
+  })
+})
