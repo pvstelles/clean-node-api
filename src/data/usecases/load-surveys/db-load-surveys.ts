302cf6a25e983ad0ed6068c8785e7d756b762f80
Pvstelles <pvstelles@gmail.com>
2023-02-11 17:15:16 -0300

    feat: ensure DbLoadSurveys call LoadSurveysRepository

---
+++ src/data/usecases/load-surveys/db-load-surveys.ts
@@ -0,0 +1,14 @@
+import { type LoadSurveys } from '../../../domain/usecases/load-surveys'
+import { type SurveyModel } from '../../../domain/models/survey'
+import { type LoadSurveysRepository } from '../../protocols/db/survey/load-surveys-repository'
+
+export class DbLoadSurveys implements LoadSurveys {
+  constructor (
+    private readonly loadSurveysRepository: LoadSurveysRepository
+  ) {}
+
+  async load (): Promise<SurveyModel[]> {
+    await this.loadSurveysRepository.loadAll()
+    return await Promise.resolve([])
+  }
+}
