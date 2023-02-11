507ac5fb92f7e287f1afd6889a317c194701816c
Pvstelles <pvstelles@gmail.com>
2023-02-11 17:05:50 -0300

    feat: ensure LoadSurveysController return 204 if LoadSurveys return empty

--- src/presentation/controller/survey/load-survey/load-surveys-controller.ts
+++ src/presentation/controller/survey/load-survey/load-surveys-controller.ts
@@ -1,5 +1,5 @@
 import { type Controller, type HttpRequest, type HttpResponse, type LoadSurveys } from './load-surveys-controller-protocols'
-import { ok, serverError } from '../../../helpers/http/http-helper'
+import { noContent, ok, serverError } from '../../../helpers/http/http-helper'
 
 export class LoadSurveysController implements Controller {
   constructor (
@@ -9,7 +9,7 @@ export class LoadSurveysController implements Controller {
   async handle (httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
       const surveys = await this.loadSurveys.load()
-      return ok(surveys)
+      return surveys.length ? ok(surveys) : noContent()
     } catch (e) {
       return serverError(e)
     }
