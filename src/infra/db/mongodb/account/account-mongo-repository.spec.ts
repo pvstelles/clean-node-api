6ce405899325def50ffe0162df458e4fb220b43f
Pvstelles <pvstelles@gmail.com>
2023-02-11 09:19:17 -0300

    test: ensure AccountMongoRepository return an account by token without role on loadByToken success

--- src/infra/db/mongodb/account/account-mongo-repository.spec.ts
+++ src/infra/db/mongodb/account/account-mongo-repository.spec.ts
@@ -71,4 +71,26 @@ describe('Account Mongo Repository', () => {
       expect(account.accessToken).toBe('any_token')
     })
   })
+  describe('loadByToken()', () => {
+    test('Should return an account by token without role on loadByToken success ', async () => {
+      await accountCollection.insertOne({
+        name: 'any_name',
+        email: 'any_email@mail.com',
+        password: 'any_password',
+        accessToken: 'any_token'
+      })
+      const sut = makeSut()
+      const account = await sut.loadByToken('any_token')
+      expect(account).toBeTruthy()
+      expect(account.id).toBeTruthy()
+      expect(account.name).toBe('any_name')
+      expect(account.email).toBe('any_email@mail.com')
+      expect(account.password).toBe('any_password')
+    })
+    // test('Should return null if loadByEmail fail', async () => {
+    //   const sut = makeSut()
+    //   const account = await sut.loadByEmail('any_email@mail.com')
+    //   expect(account).toBeFalsy()
+    // })
+  })
 })
