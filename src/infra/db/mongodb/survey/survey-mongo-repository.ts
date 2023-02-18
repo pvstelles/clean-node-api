eedeb99a7702c3dc4d6317c6cfc9899dc981c496
Pvstelles <pvstelles@gmail.com>
2023-02-18 13:43:52 -0300

    refactor: clean code

--- src/infra/db/mongodb/survey/survey-mongo-repository.ts
+++ src/infra/db/mongodb/survey/survey-mongo-repository.ts
@@ -1,16 +1,14 @@
 import { type AddSurveyRepository } from '@/data/protocols/db/survey/add-survey-repository'
 import { type AddSurveyModel } from '@/domain/usecases/add-survey'
 import { MongoHelper } from '../helpers/mongo-helper'
-import { type InsertOneResult, type Document } from 'mongodb'
 import { type LoadSurveysRepository } from '@/data/protocols/db/survey/load-surveys-repository'
 import { type SurveyModel } from '@/domain/models/survey'
 
 export class SurveyMongoRepository implements AddSurveyRepository, LoadSurveysRepository {
   async add (surveyData: AddSurveyModel): Promise<void> {
     const surveyCollection = await MongoHelper.getCollection('surveys')
-    const result: InsertOneResult<Document> = await surveyCollection.insertOne(surveyData)
-    const account: any = await surveyCollection.findOne({ _id: result.insertedId })
-    return MongoHelper.map(account)
+    const result = await surveyCollection.insertOne(surveyData)
+    return MongoHelper.map(result.ops[0])
   }
 
   async loadAll (): Promise<SurveyModel[]> {
