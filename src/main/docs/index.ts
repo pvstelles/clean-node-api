bafd83c07a15034ec25fe32fd37ff13084b52184
Pvstelles <pvstelles@gmail.com>
2023-02-20 09:18:48 -0300

    docs: add login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,8 +1,25 @@
+import { loginPath } from './paths/login-path'
+import { accountSchema } from '@/main/docs/schemas/account-schema'
+import { loginParamsSchema } from '@/main/docs/schemas/login-params-schema'
+
 export default {
   openapi: '3.0.0',
   info: {
     title: 'Clean Node API',
     description: 'Api de Boilerplate',
     version: '1.0.0'
+  },
+  servers: [
+    { url: '/api' }
+  ],
+  tags: [{
+    name: 'Login'
+  }],
+  paths: {
+    '/login': loginPath
+  },
+  schemas: {
+    account: accountSchema,
+    loginParams: loginParamsSchema
   }
 }
