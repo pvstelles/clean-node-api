e7a51d6ba44944ca3faea0033c44430119c137cb
Pvstelles <pvstelles@gmail.com>
2023-02-20 09:37:58 -0300

    docs: update login api docs

--- src/main/docs/index.ts
+++ src/main/docs/index.ts
@@ -1,6 +1,14 @@
 import { loginPath } from './paths/login-path'
 import { accountSchema } from '@/main/docs/schemas/account-schema'
 import { loginParamsSchema } from '@/main/docs/schemas/login-params-schema'
+import { errorSchema } from '@/main/docs/schemas/error-schema'
+import { badRequest } from '@/main/docs/components/bad-request'
+import { serverError } from '@/main/docs/components/server-error'
+import { unauthorized } from '@/main/docs/components/unauthorized'
+import { notFound } from '@/main/docs/components/not-found'
+import { serverErrorSchema } from '@/main/docs/schemas/server-error-schema'
+import { unauthorizedSchema } from '@/main/docs/schemas/unauthorized'
+import { notFoundSchema } from '@/main/docs/schemas/not-found-schema'
 
 export default {
   openapi: '3.0.0',
@@ -8,6 +16,10 @@ export default {
     title: 'Clean Node API',
     description: 'Api de Boilerplate',
     version: '1.0.0'
+  },
+  license: {
+    name: 'GPL-3.0-or-later',
+    url: 'https://spdx.org/licenses/GPL-3.0-or-later.html'
   },
   servers: [
     { url: '/api' }
@@ -20,6 +32,16 @@ export default {
   },
   schemas: {
     account: accountSchema,
-    loginParams: loginParamsSchema
+    loginParams: loginParamsSchema,
+    error: errorSchema,
+    serverError: serverErrorSchema,
+    unauthorized: unauthorizedSchema,
+    notFound: notFoundSchema
+  },
+  components: {
+    badRequest,
+    serverError,
+    unauthorized,
+    notFound
   }
 }
---
+++ src/main/docs/schemas/not-found-schema.ts
@@ -0,0 +1,8 @@
+export const notFoundSchema = {
+  type: 'object',
+  properties: {
+    error: {
+      type: 'string'
+    }
+  }
+}
