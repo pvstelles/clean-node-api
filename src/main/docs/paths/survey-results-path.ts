9acc81c1ee45dd93d78d73485efc32f95871915d
Pvstelles <pvstelles@gmail.com>
2023-02-25 16:59:16 -0300

    docs: add load survey result path to swagger

--- src/main/docs/paths/survey-results-path.ts
+++ src/main/docs/paths/survey-results-path.ts
@@ -43,5 +43,41 @@ export const surveyResultsPath = {
         $ref: '#/components/serverError'
       }
     }
+  },
+  get: {
+    security: [
+      { apiKeyAuth: [] }
+    ],
+    tags: ['Enquete'],
+    parameters: [{
+      in: 'path',
+      name: 'surveyId',
+      required: true,
+      schema: {
+        type: 'string'
+      }
+    }],
+    summary: 'API para consultar o resultado de uma enquete',
+    responses: {
+      200: {
+        description: 'Sucesso',
+        content: {
+          'application/json': {
+            schema: {
+              $ref: '#/schemas/surveyResult'
+            }
+          }
+        }
+      },
+      403: {
+        $ref: '#/components/forbidden'
+      },
+      404: {
+        $ref: '#/components/notFound'
+      },
+      500: {
+        $ref: '#/components/serverError'
+      }
+    }
   }
 }
