d9cbcc6c98a6211cb847c73f485bcef6c28c90e7
Pvstelles <pvstelles@gmail.com>
2023-02-25 16:49:06 -0300

    feat: ensure SurveyResultRoutes return status code 403 on load survey result without valid accessToken

--- src/main/routes/survey-result-routes.ts
+++ src/main/routes/survey-result-routes.ts
@@ -4,8 +4,13 @@ import { auth } from '../middlewares/auth'
 import {
   makeSaveSurveyResultController
 } from '@/main/factories/controllers/survey-result/save-survey-result/save-survey-result-controller-factory'
+import {
+  makeLoadSurveyResultController
+} from '@/main/factories/controllers/survey-result/load-survey-result/load-survey-result-controller-factory'
 
 export default (router: Router): void => {
   // eslint-disable-next-line @typescript-eslint/no-misused-promises
   router.put('/surveys/:surveyId/results', auth, adaptRoute(makeSaveSurveyResultController()))
+  // eslint-disable-next-line @typescript-eslint/no-misused-promises
+  router.get('/surveys/:surveyId/results', auth, adaptRoute(makeLoadSurveyResultController()))
 }
